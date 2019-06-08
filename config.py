c399602d76f9a1dd9715fd5eedd381d3c9023a6e
Silver <silveron.silveron@mail.ru>
2019-06-08 07:11:20 -0700

    fix a bug

--- config.py
+++ config.py
@@ -1,12 +1,6 @@
 from kivy.uix.label import Label
 
-
-settings_fields = {
-    "Answer label color": "[0, 1, 0, 1]"
-}
-
-
 operations = ['+', "-", "*", "/", "(", ")"]
 more_functions = ["cos", "sin", "tan", "atan"]
-answer_label = Label(color=settings_fields["Answer label color"], font_size=50)
+answer_label = Label(color=[0, 1, 0, 1], font_size=50)
 history_file = "history.txt"
